c16e4335d67e7738391e0bbe12527765a933b20f
Vinícius Machado Araújo Santana <viniksantana5@gmail.com>
2020-03-23 23:47:49 -0300

    Setting up communities
    Account creates community
    Setting up community links and views

---
+++ app/controllers/communities_controller.rb
@@ -0,0 +1,36 @@
+class CommunitiesController < ApplicationController
+  before_action :authenticate_account!, except: [:index, :show]
+
+  def index
+    @communities = Community.all
+  end
+
+  def show
+    set_community
+  end
+
+  def new
+    @community = Community.new
+  end
+
+  def create
+    @community = Community.new community_values
+    @community.account_id = current_account.id
+
+    if @community.save
+      redirect_to communities_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_community
+    @community = Community.find(params[:id])
+  end
+
+  def community_values
+    params.require(:community).permit(:name, :url, :rules)
+  end
+end
